a5d91e3f4d212d5b87f93576b5e6909741bc082a
admin <jisok@hanmail.net>
2017-05-14 14:56:07 +0900

    added complete

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import redirect
+from .forms import PostForm
 
 def post_list(request):
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -17,10 +18,24 @@ def post_new(request):
         form=PostForm(request.POST)
         if form.is_valid():
             post=form.save(commit=False)
-            post.author = request.User
-            post.published_date= request.timezone.now()
+            post.author = request.user
+            post.published_date= timezone.now()
             post.save()
-            return redirect('post_detail.html', pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form=PostForm()
-    return render(request, 'blog/post_edit', {'form':form})
+    return render(request, 'blog/post_edit.html', {'form':form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
